3736316bf25ef2d97c9111ab420bff28a4f7f8d6
Davide Bertolotto <davide.bertolotto@gmail.com>
2019-11-26 00:21:05 +0100

    fix start script

--- start-debian.sh
+++ start-debian.sh
@@ -1,3 +1,16 @@
 unset LD_PRELOAD
-#!/data/data/com.termux/files/usr/bin/sh
-  proot -0 -r ~/debian/stable -b /dev/ -b /sys/ -b /proc/ -w /root /usr/bin/env -i HOME=/root TERM="xterm-256color" PS1='[root@stable \W]$ ' PATH=/bin:/usr/bin:/sbin:/usr/sbin:/bin /bin/bash --login
+proot \
+	-0 \
+	-r ~/debian/stable \
+	-b /dev/ \
+	-b /sys/ \
+	-b /proc/ \
+	-w /root \
+	--link2symlink \
+	/usr/bin/env -i \
+	HOME=/root \
+	SHELL=/bin/bash \
+	TERM="xterm-256color" \
+	PS1='[root@stable \W]$ ' \
+	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/bin \
+	/bin/bash --login
